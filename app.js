41c35ad2146c51ecda72a7e27b626d32f5317e60
venkat-fission <ramana.gulla@fissionlabs.com>
2022-04-01 18:20:43 +0530

    added body parser

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser')
 const port = 8080;
+
+//app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
 
 app.listen(port,async()=> {
     console.log(`The server is running under port ${port}`)
@@ -10,3 +16,9 @@ app.listen(port,async()=> {
 app.get('/',(req,res)=>{
     res.send('This is sample EC2 example');
   })
+
+  app.post('/user',(req,res)=>{      
+    let obj =req.body;
+    obj.status = 200;
+    res.send(obj);
+  })  
